8bfbe142f4869b815ca600c19a0ee3da980aa368
phamtanphat <phatdroid94@gmail.com>
2023-12-19 20:35:28 +0700

    set get livedata

--- app/src/main/java/com/example/viewmodelandlivedata11092023/MainActivity.kt
+++ app/src/main/java/com/example/viewmodelandlivedata11092023/MainActivity.kt
@@ -2,13 +2,26 @@ package com.example.viewmodelandlivedata11092023
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val test = MyLiveData()
-        test.numberLiveData.value
+        val myInstance = MyLiveData()
+
+        // Get value from Live data
+        myInstance.getNumberLiveData().observe(this) {
+            Log.d("pphat", it.toString())
+        }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            myInstance.setNumberLiveData(Random.nextInt(10))
+        }, 2000)
     }
 }
